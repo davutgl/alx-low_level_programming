4c4603294c97d37621ef8c6787b24c77ba22d94e
davutgl <glordvt@gmail.com>
2023-03-17 09:04:18 -0700

    5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Prints decimal digits.
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	i = 10;
+	while (i < 10) {
+		printf("%d\n", i);
+		i++;
+	}
+	return (0);
+}
