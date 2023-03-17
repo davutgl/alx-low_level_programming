b8643bab3174c993a09d8e70fa276130403b820b
davutgl <glordvt@gmail.com>
2023-03-17 08:32:39 -0700

    1

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -8,9 +8,9 @@
 int main(void)
 {
 	char lower;
-	for (lower = 'a'; lower <= 'z'; lower++){
+	for (lower = 'a'; lower <= 'z'; lower++)
+
 		putchar(lower);
-	}
 	putchar('\n');
 	return (0);
 }
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -9,12 +9,12 @@ int main(void)
 {
 	char lc;
 	char uc;
-	for (lc = 'a'; lc <= 'z'; lc++){
+	for (lc = 'a'; lc <= 'z'; lc++)
+
 		putchar(lc);
-	}
-	for (uc = 'A'; uc <= 'Z'; uc++){
+	for (uc = 'A'; uc <= 'Z'; uc++)
+
 		putchar(uc);
-	}
 	putchar('\n');
 	return (0);
 }
