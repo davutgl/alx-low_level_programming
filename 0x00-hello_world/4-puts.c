5257729c72f79ef51b1a6c02f9c89915a59a5d27
davutgl <glordvt@gmail.com>
2023-03-17 04:06:12 -0700

    4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+/* function main : starting point for programme execution. */
 int main(void)
 {
-puts("\"Programming is like building a multilingual puzzle");
-return(0);
+	puts("\"Programming is like building a multilingual puzzle");
+	return(0);
 }
