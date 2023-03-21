39ede4ac6b8165849e8b24b7fb08341ec6095e05
davutgl <glordvt@gmail.com>
2023-03-21 12:23:33 -0700

    0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,13 @@
+#include <main.h>
+/**
+ * main - Prints putchar.
+ *
+ * Return: Always 1 (Success)
+ */
+int main(void)
+{
+	int _putchar(char _putchar)
+	{
+		return (write(1, &_putchar, 1));
+	}
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Tasks on C functions and nested loops.
