9d94e8dcb6bc9b6701654a2879e048c289252704
davutgl <glordvt@gmail.com>
2023-03-23 16:14:21 -0700

    4

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -6,17 +6,29 @@
  */
 int main(void)
 {
-        int i;
+	int n;
 
-        for (i = 1; i <= 100; i++)
-        {
-                if (i % 3 == 0)
-                {
-                        printf("Fizz\t");
-                }
-                else if (i % 5 == 0)
-                {
-                        printf("Buzz\t");
-                }
-                else if (i
+	n = 1;
+	printf("%d", n);
+	for (n = 2; n <= 100; n++)
+	{
+		if ((n % 3 == 0) && (n % 5 == 0))
+		{
+			printf(" FizzBuzz");
+		}
+		else if (n % 3 == 0)
+		{
+			printf(" Fizz");
+		}
+		else if (n % 5 == 0)
+		{
+			printf(" Buzz");
+		}
+		else
+		{
+			printf(" %d", n);
+		}
+	}
+	printf("\n");
+	return (0);
 }
