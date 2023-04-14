0f86352084806d7ee727c19cc7030e27943e3a78
davutgl <glordvt@gmail.com>
2023-04-14 14:41:17 -0700

    1

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -2,14 +2,13 @@
 #include <stdio.h>
 
 /**
- * main - check the code for ALX SE School students.
+ * main - check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
 	char buffer[] = "This is a string!\0And this is the rest of the #buffer :)\1\2\3\4\5\6\7#cisfun\n\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\0\x20\x21\x34\x56#pointersarefun #infernumisfun\n";
-	/*char buffer[] = "This is th";*/
 
 	printf("%s\n", buffer);
 	printf("---------------------------------\n");
